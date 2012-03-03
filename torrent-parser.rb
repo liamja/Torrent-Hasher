8ada5c29d1c3a85d2936de81dbc757dcf1b19960
Liam Anderson <liam.anderson.91@gmail.com>
2012-03-03 16:37:57 +0000

    spits out info hash

--- torrent-parser.rb
+++ torrent-parser.rb
@@ -2,5 +2,9 @@ require 'rubygems'
 require 'bencode'
 require 'digest/sha1'
 
-torrent = BEncode.load_file("the-muppets.torrent")
-puts torrent["announce"]
+# Hash to get
+# 19f090af75ac25bcb3e1835624431a218874a3fd
+
+torrent		= BEncode.load_file( "the-muppets.torrent" )
+info_hash	= Digest::SHA1.hexdigest( torrent['info'].bencode )
+puts info_hash
