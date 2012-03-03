27039ca0864a20b5cb7def6e39551743b4ccd8a9
Liam Anderson <liam.anderson.91@gmail.com>
2012-03-03 16:53:57 +0000

    removed the optparse livrary requirement

--- torrent-parser.rb
+++ torrent-parser.rb
@@ -1,12 +1,11 @@
 require 'rubygems'
 require 'bencode'
 require 'digest/sha1'
-require 'optparse'
 
 ARGV.each do|file|
-	puts "File: #{file}"
 	torrent		= BEncode.load_file( file )
 	info_hash	= Digest::SHA1.hexdigest( torrent['info'].bencode )
+	puts "File: #{file}"
 	puts "Hash: #{info_hash}"
 	puts "---"
 end
